56aff6467aa2ddea1148c8fc05bbc4d7422dd4d7
RapeephatPp <rapeephat.prat@bumail.net>
2026-02-06 16:40:57 +0700

    Week4 Commit
    Add new prefabs (Dog1/2/3 variants, Food) and related .meta files, plus IDE/project settings. Update Enemy1 prefab (rename and increase speed). Implement player input handling and shooting in PlayerController (movement, x-range clamp, shoot to spawn food, gizmos), make MoveForward translate by speed, and add outOfBound limit logic to DestroyOutOfBound to remove objects outside Z bounds. Add SpawnManager support for spawning random dog prefabs via the S key.

--- Assets/Scripts/DestroyOutOfBound.cs
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 public class DestroyOutOfBound : MonoBehaviour
 {
     // Update is called once per frame
+    public int outOfBound = 20;
     void Update()
     {
-
+        if (transform.position.z > outOfBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < -outOfBound)
+        {
+               Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,10 +1,24 @@
+using System;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
-{
+{   
+    public GameObject [] dogPrefabs;
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            float x = UnityEngine.Random.Range(-10f, 10f);
+            int index = UnityEngine.Random.Range(0, dogPrefabs.Length);
+            if (dogPrefabs[index] != null)
+            {
+                Instantiate(dogPrefabs[index], new Vector3(x, 0, 20), Quaternion.Euler(0, 180, 0));
+            }
+            else
+            {
+                Debug.LogWarning("No dog prefab found");
+            }
+        }
     }
 }
